d3510d7c169979f0089ac0f13e799d87475e51b4
augusto <august@euland-pc.com>
2020-10-25 22:17:28 -0300

    é isso

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def fibonacci():
         found=found+1
         resposta+= str(proximo) + ", "
 
-        return resposta
+    return resposta
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
